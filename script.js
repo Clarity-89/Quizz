29911a5af6d13d95afbb517fb82fdbe7c732d04c
Alex <homes89@ukr.net>
2015-02-17 00:00:04 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Alex on 14/2/15.
+ */
+/*global $:false */
+$(document).ready(function(){
+    var allQuestions = [{
+    question: "Who is Prime Minister of the United Kingdom?",
+    choices: ["David Cameron", "Gordon Brown", "Tony Blair", "Winston Churchill"],
+    corAnswer: 0
+}, {
+    question: "Which country has the biggest population?",
+    choices: ["Finland", "China", "India", "USA"],
+    corAnswer: 1
+}, {
+    question: "Which country does not belong to Scandinavia?",
+    choices: ["Sweden", "Finland", "Denmark", "Iceland"],
+    corAnswer: 3
+}];
+    var numCorrect = 0;
+    var numQuestion = 0;
+       var display = function() {
+        if (numQuestion === allQuestions.length) {
+            $('h2').text('You are done with the quiz, your score is ' + numCorrect);
+            $('h1, #form').remove();
+        } else {
+            $('h2').text(allQuestions[numQuestion].question);
+            for (var i = 0; i <= allQuestions.length; i++) {
+               $('#a'+(i+1)).text(allQuestions[numQuestion].choices[i]);
+           }
+        }
+        };
+    display();
+$('#but').click(function(){
+    var correctChoice = allQuestions[numQuestion].corAnswer;
+    var answer = $('input[name=Answer]:checked').val();
+    if (answer == correctChoice) {
+        numCorrect++;
+    }
+    numQuestion++;
+    display();
+   });
+$('#prev').click(function(){
+        numQuestion--;
+    display();
+    });
+});
