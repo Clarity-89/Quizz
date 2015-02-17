4d5f73e9cde27e0807518f2c8c6264179b61db23
Alex <homes89@ukr.net>
2015-02-17 22:37:22 +0200

    Added jQuery animation

--- script.js
+++ script.js
@@ -16,37 +16,63 @@ $(document).ready(function () {
         choices: ["Sweden", "Finland", "Denmark", "Iceland"],
         corAnswer: 3
     }];
+    var savedAnswer = allQuestions.forEach(function (indx) {
+        indx.usersAnswer = null;
+    });
     var numCorrect = 0;
     var numQuestion = 0;
+    $('h1').hide().fadeIn('slow');
     var display = function () {
+                if (numQuestion > 0) {
+            $('#prev').show();
+        }
         if (numQuestion === allQuestions.length) {
-            $('h2').text('You are done with the quiz, your score is ' + numCorrect);
-            $('h1, #form').remove();
+            $('h2').text('You are done with the quiz, your score is ' + numCorrect).fadeIn('slow');
+            $('h1, #form').fadeOut('slow').remove();
         } else {
             $('h2').text(allQuestions[numQuestion].question);
             for (var i = 0; i <= allQuestions.length; i++) {
-                $('#a' + (i + 1)).text(allQuestions[numQuestion].choices[i]);
+              $('#a' + (i + 1)).text(allQuestions[numQuestion].choices[i]);
             }
         }
+
         $('input[name=Answer]').attr('checked', false);
     };
     display();
+    fading();
+    function fading (){
+        $('h2').hide().fadeIn();
+        for (var i = 0; i <=allQuestions.length; i++) {
+            $('div').not('#next').hide().fadeIn();
+        }
+    }
+    fading();
     $('#next').click(function () {
         var correctChoice = allQuestions[numQuestion].corAnswer;
         var answer = $('input[name=Answer]:checked').val();
+        allQuestions[numQuestion].usersAnswer = answer;
+        console.log(allQuestions); //log the usersAnswer to check if they save correctly
         if ($("input:radio[name=Answer]").is(":checked") === false) {
-            alert("Make a choice!");
+            alert("You didn't select your answer!");
             return false;
         } else if (answer == correctChoice) {
             numCorrect++;
         }
         numQuestion++;
+        fading();
+        console.log(numQuestion);
         display();
+
     });
     $('#prev').click(function () {
+        fading();
         numQuestion--;
         if (numQuestion < 0) {
             numQuestion = 0;
+        }
+        console.log(numQuestion);
+        if (numQuestion === 0){
+            $('#prev').css({"display": "none"});
         }
         display();
     });
