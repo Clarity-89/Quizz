7230a72521bde7d22d1e04339b4fbadb3d5eb546
Alex <homes89@ukr.net>
2015-02-17 14:13:58 +0200

    Updates

--- script.js
+++ script.js
@@ -2,52 +2,52 @@
  * Created by Alex on 14/2/15.
  */
 /*global $:false */
-$(document).ready(function(){
+$(document).ready(function () {
     var allQuestions = [{
-    question: "Who is Prime Minister of the United Kingdom?",
-    choices: ["David Cameron", "Gordon Brown", "Tony Blair", "Winston Churchill"],
-    corAnswer: 0
-}, {
-    question: "Which country has the biggest population?",
-    choices: ["Finland", "China", "India", "USA"],
-    corAnswer: 1
-}, {
-    question: "Which country does not belong to Scandinavia?",
-    choices: ["Sweden", "Finland", "Denmark", "Iceland"],
-    corAnswer: 3
-}];
+        question: "Who is Prime Minister of the United Kingdom?",
+        choices: ["David Cameron", "Gordon Brown", "Tony Blair", "Winston Churchill"],
+        corAnswer: 0
+    }, {
+        question: "Which country has the biggest population?",
+        choices: ["Finland", "China", "India", "USA"],
+        corAnswer: 1
+    }, {
+        question: "Which country does not belong to Scandinavia?",
+        choices: ["Sweden", "Finland", "Denmark", "Iceland"],
+        corAnswer: 3
+    }];
     var numCorrect = 0;
     var numQuestion = 0;
-          var display = function() {
+    var display = function () {
         if (numQuestion === allQuestions.length) {
             $('h2').text('You are done with the quiz, your score is ' + numCorrect);
             $('h1, #form').remove();
         } else {
             $('h2').text(allQuestions[numQuestion].question);
             for (var i = 0; i <= allQuestions.length; i++) {
-               $('#a'+(i+1)).text(allQuestions[numQuestion].choices[i]);
-           }
+                $('#a' + (i + 1)).text(allQuestions[numQuestion].choices[i]);
+            }
         }
-            $('input[name=Answer]').attr('checked',false);
-        };
+        $('input[name=Answer]').attr('checked', false);
+    };
     display();
-$('#next').click(function(){
-    var correctChoice = allQuestions[numQuestion].corAnswer;
-    var answer = $('input[name=Answer]:checked').val();
-    if ($("input:radio[name=Answer]").is(":checked") === false) {
-        alert("Make a choice!");
-        return false;
-                } else if (answer == correctChoice) {
-        numCorrect++;
-    }
-    numQuestion++;
-    display();
+    $('#next').click(function () {
+        var correctChoice = allQuestions[numQuestion].corAnswer;
+        var answer = $('input[name=Answer]:checked').val();
+        if ($("input:radio[name=Answer]").is(":checked") === false) {
+            alert("Make a choice!");
+            return false;
+        } else if (answer == correctChoice) {
+            numCorrect++;
+        }
+        numQuestion++;
+        display();
     });
-$('#prev').click(function(){
+    $('#prev').click(function () {
         numQuestion--;
-    if (numQuestion < 0){
-        numQuestion = 0;
-    }
-    display();
+        if (numQuestion < 0) {
+            numQuestion = 0;
+        }
+        display();
     });
-    });
+});
