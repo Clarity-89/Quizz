cde006a34c778e54e6e645aded44c51f0c2d0529
Alex <homes89@ukr.net>
2015-02-19 17:57:01 +0200

    Fixed back button issue, updated html for labels

--- script.js
+++ script.js
@@ -16,14 +16,14 @@ $(document).ready(function () {
         choices: ["Sweden", "Finland", "Denmark", "Iceland"],
         corAnswer: 3
     }];
-    var savedAnswer = allQuestions.forEach(function (indx) {
+    allQuestions.forEach(function (indx) {
         indx.usersAnswer = null;
     });
     var numCorrect = 0;
     var numQuestion = 0;
     $('h1').hide().fadeIn('slow');
     var display = function () {
-                if (numQuestion > 0) {
+        if (numQuestion > 0) {
             $('#prev').show();
         }
         if (numQuestion === allQuestions.length) {
@@ -32,11 +32,11 @@ $(document).ready(function () {
         } else {
             $('h2').text(allQuestions[numQuestion].question);
             for (var i = 0; i <= allQuestions.length; i++) {
-              $('#a' + (i + 1)).text(allQuestions[numQuestion].choices[i]);
+                $('#b' + (i + 1)).text(allQuestions[numQuestion].choices[i]);
             }
         }
 
-        $('input[name=Answer]').attr('checked', false);
+
     };
     display();
     fading();
@@ -53,7 +53,7 @@ $(document).ready(function () {
         allQuestions[numQuestion].usersAnswer = answer;
         console.log(allQuestions); //log the usersAnswer to check if they save correctly
         if ($("input:radio[name=Answer]").is(":checked") === false) {
-            alert("You didn't select your answer!");
+            alert("Please make your choice!");
             return false;
         } else if (answer == correctChoice) {
             numCorrect++;
@@ -62,11 +62,13 @@ $(document).ready(function () {
         fading();
         console.log(numQuestion);
         display();
+        $('input[name=Answer]').attr('checked', false);
 
     });
     $('#prev').click(function () {
         fading();
         numQuestion--;
+        numCorrect--;
         if (numQuestion < 0) {
             numQuestion = 0;
         }
@@ -74,6 +76,11 @@ $(document).ready(function () {
         if (numQuestion === 0){
             $('#prev').css({"display": "none"});
         }
+        var showPrev = function () {
+            var j = allQuestions[numQuestion].usersAnswer;
+            $('input[type="radio"][value="' + j + '"]').prop('checked', true);
+        };
+        showPrev();
         display();
     });
 });
